ad94a687c724870951dd66a543335fa36b99a30f
bkvaale <bkvaale@gmail.com>
2015-02-12 11:29:03 +0100

    Removed driver test directory

--- DriverTestGo/Driver_Test.go
+++
@@ -1,7 +0,0 @@
-package Driver_Test  // where "driver" is the folder that contains io.go, io.c, io.h, channels.go, channels.c and driver.go
-/*
-#cgo LDFLAGS: -lcomedi -lm
-#include "io.h"
-#include "elev.h"
-*/
-import "C"
--- DriverTestGo/main.go
+++
@@ -1,9 +0,0 @@
-export GOPATH="/home/bjorkv/Desktop/go"
-
-package main
-
-import "Driver_Test"
-
-func main(){
-	C.elev_set_motor_direction(C.DIRN_UP);
-}
