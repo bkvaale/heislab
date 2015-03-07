77d029fa8ff39c7298212919d41ee3ec36ea4180
bkvaale <bkvaale@gmail.com>
2015-03-07 11:50:57 +0100

    changed network from three seperate files to one general file

--- src/network/networkTestGeneral.go
+++ src/network/networkTestGeneral.go
@@ -3,48 +3,62 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
-	//"os"
+	"os"
 	"time"
 	"strings"
+	"strconv"
 )
 
 
-
-func RunElev1() {
+// elev 1 uses ports :12010 and :12011, elev 2 :12012 and :12013, elev 3 :12014 and :12015
+func RunElev(elev int,numElev int) {
 	fmt.Println("Starting server!")
 	done := make(chan bool)
-	broadcastAddr := "129.241.187.255:12010" // connecting to elevator 2
-	//broadcastAddr := "localhost:12000" //localhost
-	listenAddr := ":12012"	// listening to elevator 2
-	lAddr, err := net.ResolveUDPAddr("udp", listenAddr)
-	CheckError(err)
-	bAddr, err := net.ResolveUDPAddr("udp", broadcastAddr)
-	CheckError(err)
-	lConn, err := net.ListenUDP("udp", lAddr)
-	CheckError(err)
-	bConn, err := net.DialUDP("udp", nil, bAddr)
-	CheckError(err)
-	go CastDataElev1(bConn)
-	go ReceiveDataElev1(lConn)
-	/*
-	broadcastAddr = "192.168.1.255:12011" // connecting to elevator 3
-	//broadcastAddr := "localhost:12000" //localhost
-	listenAddr = ":12014"	// listening to elevator 3
-	lAddr, err = net.ResolveUDPAddr("udp", listenAddr)
-	CheckError(err)
-	bAddr, err = net.ResolveUDPAddr("udp", broadcastAddr)
-	CheckError(err)
-	lConn, err = net.ListenUDP("udp", lAddr)
-	CheckError(err)
-	bConn, err = net.DialUDP("udp", nil, bAddr)
-	CheckError(err)
-	go CastDataElev1(bConn)
-	go ReceiveDataElev1(lConn)
-	*/
+	port := 0
+	switch elev{
+	case 1:
+		port = 12010
+		break
+	case 2:
+		port = 12012
+		break
+	case 3:
+		port = 12014
+		break
+	}
+	for i:=0; i<numElev-1; i++{
+		port+=i
+		portString := strconv.Itoa(port)
+		broadcastAddr := "129.241.187.255:"+portString // connecting to elevator 2
+		//broadcastAddr := "localhost:12000" //localhost
+		listenAddr:=""
+		if(i==0 && elev==1){
+			listenAddr = ":12012"
+		}else if(i==1 && elev==1){
+			listenAddr = ":12014"
+		}else if(i==0 && elev==2){
+			listenAddr = ":12010"
+		}else if(i==1 && elev==2){
+			listenAddr = ":12015"
+		}else if(i==0 && elev==3){
+			listenAddr = ":12011"
+		}else if(i==1 && elev==3){
+			listenAddr = ":12013"
+		}
+		lAddr, err := net.ResolveUDPAddr("udp", listenAddr)
+		CheckError(err)
+		bAddr, err := net.ResolveUDPAddr("udp", broadcastAddr)
+		CheckError(err)
+		lConn, err := net.ListenUDP("udp", lAddr)
+		CheckError(err)
+		bConn, err := net.DialUDP("udp", nil, bAddr)
+		CheckError(err)
+		go CastData(bConn,elev)
+		go ReceiveData(lConn,elev)
+	}
 	fmt.Println("Sockets created successfully Elev 1")
 	<-done
 }
-/*
 func CheckError(err error) {
 		if err != nil {
 		if(strings.Contains(err.Error(),"use of closed network connection")){
@@ -85,23 +99,13 @@ func CheckErrorUDP(err error, conn *net.UDPConn) bool {
 	}
 	return false
 }
-*/
-/*
-func checkPeerLife(msg Message) bool{
-	tdiff := time.Since(msg.CurrTime)
-	if(tdiff > 10*time.Second){	// 2 second timeout
-		fmt.Println("Connection to elev", msg.ID, " was lost!, time diff: " , tdiff)
-		return false
-	}
-	return true
-}
-*/
-func ReceiveDataElev1(conn *net.UDPConn) {
+
+func ReceiveData(conn *net.UDPConn, elev int) {
 	var msg Message
 	//var err error
 	var b = make([]byte, 1024)
 	time.Sleep(10*time.Second)
-	fmt.Println("Starting elev1!")
+	fmt.Println("Starting elev" , elev)
 	for {
 			//fmt.Println("msg: " , msg)
 			//checkPeerLife(msg)
@@ -112,7 +116,7 @@ func ReceiveDataElev1(conn *net.UDPConn) {
 			if(CheckErrorUDP(err,conn)==false){
 				err = json.Unmarshal(b[:n], &msg)
 				CheckError(err)
-				fmt.Println("Data Received on Elev1:", msg)
+				fmt.Println("Data Received on Elev", elev, msg)
 			}else if(strings.Contains(err.Error(),"use of closed network connection")){
 				fmt.Println("stopping function")
 				break	// stop the function
@@ -120,15 +124,15 @@ func ReceiveDataElev1(conn *net.UDPConn) {
 		}
 }
 
-func CastDataElev1(conn *net.UDPConn) {
+func CastData(conn *net.UDPConn, elev int) {
 	var msg Message
 	var err error
 	for {
 		//msg = <-OutputCh
 		//msg.ID = types.CART_ID
 		msg.CurrTime = time.Now()
-		msg.Word = "Message from Elev1!"
-		msg.ID = "elev 1"
+		msg.Word = "Message from Elev"+strconv.Itoa(elev)
+		msg.ID = "elev"
 		for i := 0; i < 1; i++ {
 			//fmt.Println("Data casted on Server:", msg)
 			b := make([]byte, 1024)
