5720de70a0ea72c7acebabbf58efb41369bcc2ed
shivjr <rai.shivjeet@gmail.com>
2015-03-08 09:40:14 +0100

    Update io.go
    Navngiving

--- src/driver/io.go
+++ src/driver/io.go
@@ -5,27 +5,27 @@ package driver  // where "driver" is the folder that contains io.go, io.c, io.h,
 */
 import "C"
 
-func IO_init() int{
+func IoInit() int{
 	return int(C.io_init())
 }
 
-func IO_set_bit(channel int){
+func IoSetBit(channel int){
 	C.io_set_bit(C.int(channel))
 }
 
-func IO_clear_bit(channel int){
+func IoClearBit(channel int){
 	C.io_clear_bit( C.int(channel))
 }
 
-func IO_write_analog(channel int, value int){
+func IoWriteAnalog(channel int, value int){
 	C.io_write_analog(C.int(channel), C.int(value))
 }
 
-func IO_read_bit(channel int) int{
+func IoReadBit(channel int) int{
 	return int(C.io_read_bit(C.int(channel)))
 }
 
-func IO_read_analog(channel int) int{
+func IoReadAnalog(channel int) int{
 	return int(C.io_read_analog(C.int(channel)))
 }
 
